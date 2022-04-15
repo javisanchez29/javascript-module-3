776ba5ae142ee81fd72eabfc2f9dd83465d3dd53
Sergi Martínez <sergi.martinez@gmail.com>
2022-04-15 12:37:48 +0200

    improved structure

--- Rick and Morty/scripts/script.js
+++ Rick and Morty/scripts/script.js
@@ -1,11 +1,34 @@
 function buildRoot() {
   const root = document.getElementById("root")
-  root.innerHTML = '<div id="header"><h1>Rick and Morty API</h1></div>' +
+  root.innerHTML =
     '<div id="content">' +
-      '<div id="sidebar"><div id="chapters"></div></div>' +
+      '<div id="sidebar"><div id="episodes"></div></div>' +
       '<div id="main"></div>' +
-    '</div>'
+    '</div>' +
+    '<div id="header"><h1>Rick and Morty API</h1></div>'
+}
+
+function loadEpisodes() {
+  fetch("https://rickandmortyapi.com/api/episode")
+    .then(res => res.json())
+    .then(episodes => showEpisodeLinks(episodes))
+    .catch(reason => console.log(reason))
+}
+
+function showEpisodeLinks(episodes) {
+  const episodesContainer = document.getElementById("episodes")
+  episodes.results
+    .map(createEpisodeLink)
+    .forEach(node => episodesContainer.appendChild(node))
+}
+
+function createEpisodeLink(episode) {
+  const episodeLink = document.createElement("div")
+  episodeLink.classList.add("episodeLink")
+  episodeLink.innerText = episode.name
+  return episodeLink
 }
 
 
 buildRoot()
+loadEpisodes()

7bedd6b219f700d94377389f4cc6f15e756d13c0
Sergi Martínez <sergi.martinez@gmail.com>
2022-04-15 13:32:09 +0200

    characters in episode

--- Rick and Morty/scripts/script.js
+++ Rick and Morty/scripts/script.js
@@ -26,8 +26,37 @@ function createEpisodeLink(episode) {
   const episodeLink = document.createElement("div")
   episodeLink.classList.add("episodeLink")
   episodeLink.innerText = episode.name
+  episodeLink.addEventListener("click", () => showEpisodeDetail(episode))
   return episodeLink
 }
+
+function showEpisodeDetail(episode) {
+  const main = document.getElementById("main")
+  main.innerHTML = `<h2>${episode.name}</h2>` +
+    `<h3>${episode.episode} | ${episode.air_date} </h3>`
+
+  const charactersContainer = document.createElement("div")
+  charactersContainer.id="charactersContainer"
+  main.appendChild(charactersContainer)
+  episode.characters
+  .forEach(characterUrl => createCharacterThumbnail(charactersContainer, characterUrl))
+}
+
+function createCharacterThumbnail(parent, characterUrl) {
+  const div = document.createElement("div")
+  div.classList.add("characterThumbnail")
+  fetch(characterUrl)
+    .then(res => res.json())
+    .then(character => renderCharacterThumbnail(div, character))
+  parent.appendChild(div)
+}
+
+function renderCharacterThumbnail(parent, character) {
+  parent.innerHTML = `<img src=${character.image}>` +
+  `<h4>${character.name}</h4>` +
+  `<h5>${character.species} | ${character.status}</h5>`
+}
+
 
 
 buildRoot()
